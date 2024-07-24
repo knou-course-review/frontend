286e47fad5f90e3c247b56d466c3296ccc138e3f
jbilee <julishahn@gmail.com>
2024-07-24 09:13:30 +0900

    feat: NavBar 컴포넌트에 홈링크 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <div className="flex border-b border-b-slate-400 bg-white justify-items-center h-28 items-center p-8">
-      <div>로고</div>
+      <div><Link href="/">로고</Link></div>
       <div className="flex gap-7 ml-auto list-none">
         <li>
           <Link href="/login">로그인</Link>
