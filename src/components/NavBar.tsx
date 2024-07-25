0b967107ae2426ef0d8d88e66eecec7f122eb982
jbilee <julishahn@gmail.com>
2024-07-25 15:43:03 +0900

    refactor: NavBar 컴포넌트 height 조정

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div className="flex border-b border-b-slate-400 bg-white justify-items-center h-28 items-center p-8">
+    <div className="flex border-b border-b-slate-400 bg-white justify-items-center h-24 items-center p-8">
       <div><Link href="/">로고</Link></div>
       <div className="flex gap-7 ml-auto list-none">
         <li>
