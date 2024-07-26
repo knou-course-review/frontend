4f2fd20bd7a32c5b1557fd73c2ae311df6042e48
jbilee <julishahn@gmail.com>
2024-07-26 17:43:31 +0900

    refactor: 라벨 컴포넌트 개선 - display 요소에 대한 값을 prop으로 전달할 수 있도록 수정

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,8 +1,9 @@
 type LabelProps = {
   text: string;
   background: string;
+  display?: string;
 };
 
-export default function Label({ text, background }: LabelProps) {
-  return <div className={`text-center text-white px-4 py-2 rounded-full ${background}`}>{text}</div>;
+export default function Label({ text, background, display = "block" }: LabelProps) {
+  return <div className={`text-center text-white px-4 py-2 rounded-full ${display} ${background}`}>{text}</div>;
 }
