f2ef2e148777c96f8337fbc4a84506a68f683fa8
jbilee <julishahn@gmail.com>
2024-07-29 12:57:18 +0900

    chore: Tailwind CSS에 커스텀 borderRadius 사이즈 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,15 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       spacing: {
         "100": "450px",
-        "200": "1030px"
-      }
+        "200": "1030px",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
     },
   },
   plugins: [],
