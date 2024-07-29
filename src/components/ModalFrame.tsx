6de6876eb0091dec64bca95e2435b794f47f0259
jbilee <julishahn@gmail.com>
2024-07-29 13:11:08 +0900

    feat: 모달창 wrapper 컨포넌트 구현

---
+++ src/components/ModalFrame.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Close from "@mui/icons-material/Close";
+import type { ReactNode } from "react";
+
+type ModalFrameProps = {
+  closeModal: () => void;
+  children: ReactNode;
+};
+
+export default function ModalFrame({ closeModal, children }: ModalFrameProps) {
+  return (
+    <div className="fixed top-0 left-0 right-0 bottom-0 grid place-content-center">
+      <div className="absolute w-full h-full opacity-50 bg-black z-10" onClick={closeModal} />
+      <div className="bg-white z-20 p-10 rounded-4xl relative">
+        <div className="absolute right-8 top-8">
+          <Close className="cursor-pointer" onClick={closeModal} />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
