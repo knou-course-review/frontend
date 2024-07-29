18e984873e5289875ecf4a6f39fd5fa05db13f0c
jbilee <julishahn@gmail.com>
2024-07-29 13:17:38 +0900

    refactor: 모달창 wrapper 컴포넌트 리팩토링 - Wrapper 컴포넌트가 모달창의 헤더 텍스트도 prop으로 받아 표시하도록 수정

--- src/components/ModalFrame.tsx
+++ src/components/ModalFrame.tsx
@@ -5,10 +5,11 @@ import type { ReactNode } from "react";
 
 type ModalFrameProps = {
   closeModal: () => void;
+  title: string;
   children: ReactNode;
 };
 
-export default function ModalFrame({ closeModal, children }: ModalFrameProps) {
+export default function ModalFrame({ closeModal, title, children }: ModalFrameProps) {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 grid place-content-center">
       <div className="absolute w-full h-full opacity-50 bg-black z-10" onClick={closeModal} />
@@ -16,6 +17,7 @@ export default function ModalFrame({ closeModal, children }: ModalFrameProps) {
         <div className="absolute right-8 top-8">
           <Close className="cursor-pointer" onClick={closeModal} />
         </div>
+        <h1 className="text-xl font-bold mb-4">{title}</h1>
         {children}
       </div>
     </div>
