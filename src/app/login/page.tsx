6d1818595cee451d3d1f48eecc577c383dcb88b7
jbilee <julishahn@gmail.com>
2024-07-31 02:41:17 +0900

    refactor: 회원가입 및 로그인 페이지 UI 개선

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Login() {
   return (
-    <div className="flex flex-col gap-4 text-center">
+    <div className="flex flex-col gap-4 p-24 text-center">
       <h1 className="font-bold text-2xl mb-8">로그인</h1>
       <LoginForm />
       <Link href="/signup">회원이 아니신가요? 회원가입 하러 가기 →</Link>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from "@/components/signup/SignUpForm";
 
 export default function Login() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 p-24">
       <h1 className="font-bold text-2xl mb-8 text-center">회원가입</h1>
       <SignUpForm />
     </div>
