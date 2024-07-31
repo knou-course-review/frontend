62c2e4d7f5dab225e7913c5a2f46b18bc6c79603
jbilee <julishahn@gmail.com>
2024-07-31 11:08:09 +0900

    feat: 인증코드 타이머 컴포넌트 구현

---
+++ src/components/signup/Timer.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { memo, useEffect, useRef } from "react";
+
+type TimerProps = {
+  endTimer: () => void;
+};
+
+export const calculateMinutes = (time: number) => {
+  return Math.floor((time - Math.floor(time / 3600) * 3600) / 60);
+};
+
+export const calculateSeconds = (time: number) => {
+  return time - Math.floor(time / 3600) * 3600 - Math.floor((time - Math.floor(time / 3600) * 3600) / 60) * 60;
+};
+
+const getTime = (ms: number) =>
+  `${calculateMinutes(ms).toString().padStart(2, "0")}:${calculateSeconds(ms).toString().padStart(2, "0")}`;
+
+const Timer = memo(function Timer({ endTimer }: TimerProps) {
+  const elem = useRef<HTMLSpanElement | null>(null);
+  let timeLeft = 60 * 5;
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      timeLeft -= 1;
+      updateTime();
+      if (timeLeft === 0) {
+        clearInterval(intervalId);
+        return endTimer();
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+  const updateTime = () => {
+    if (!elem.current) return;
+    (elem.current as HTMLElement).innerText = getTime(timeLeft);
+  };
+  return <span ref={elem}>{getTime(timeLeft)}</span>;
+});
+
+export default Timer;
