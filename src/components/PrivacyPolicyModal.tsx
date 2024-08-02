117796143c45688f42544e325200a582a1ae5945
jbilee <julishahn@gmail.com>
2024-08-02 14:40:21 +0900

    feat: 개인정보 처리방침 모달창 컴포넌트 구현

---
+++ src/components/PrivacyPolicyModal.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import ModalFrame from "./ModalFrame";
+
+type PrivacyPolicyModalProps = {
+  isShowing: boolean;
+  closeModal: () => void;
+};
+
+export default function PrivacyPolicyModal({ isShowing, closeModal }: PrivacyPolicyModalProps) {
+  if (!isShowing) return null;
+  return (
+    <ModalFrame title="개인정보 처리방침" closeModal={closeModal}>
+      <div className="w-100 h-52 p-4 overflow-hidden rounded-xl border bg-slate-100 border-slate-300">
+        <div className="overflow-auto w-full h-full">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis incidunt exercitationem eveniet alias ullam
+          sequi similique inventore, non natus quas nisi harum minus, sit ratione, veritatis error eum distinctio ab
+          maxime tempora. Praesentium odio laboriosam ipsam aliquam quaerat. Nisi quos dolorem consequuntur. Placeat
+          officia optio ab. Nam iste ipsum at? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ex
+          id, amet ducimus alias eveniet aperiam dicta magni accusamus qui quo enim culpa nihil officia veritatis ad
+          odio quaerat commodi laboriosam voluptatibus perferendis eligendi molestiae sequi. Consequuntur, quaerat.
+          Minima quibusdam minus nostrum quis, corrupti placeat labore iste! Doloremque, hic eveniet!
+        </div>
+      </div>
+    </ModalFrame>
+  );
+}
