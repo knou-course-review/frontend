fe4b32c72c59811f0656c82e46369e311a51c608
jbilee <julishahn@gmail.com>
2024-08-06 22:49:04 +0900

    feat: /api/courses 루트에 페이징 조회 추가

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -4,10 +4,21 @@ import { api } from "@/utils/api";
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const idQuery = searchParams.get("id");
+  const pageQuery = searchParams.get("page");
 
   if (idQuery) {
     try {
       const res = await api.get(`/api/v1/course/${idQuery}`);
+      if (res.status === 200) {
+        const body = await res.json();
+        return NextResponse.json(body.data);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (pageQuery) {
+    try {
+      const res = await api.get(`/api/v2/courses?page=${pageQuery}`);
       if (res.status === 200) {
         const body = await res.json();
         return NextResponse.json(body.data);
