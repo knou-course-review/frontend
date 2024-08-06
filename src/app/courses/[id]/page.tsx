d440e1b6beda02504f40b69c192e5dbe96f159eb
jbilee <julishahn@gmail.com>
2024-08-07 00:24:29 +0900

    fix: 빌드 오류 수정 - 삭제된 파일에 대한 import를 코드에서 제외

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -1,33 +1,42 @@
 "use client";
 
-import { Warning } from "@mui/icons-material";
-import { Button } from "@mui/material";
-import CourseInfo from "@/components/course/CourseInfo";
-import UserReview from "@/components/course/UserReview";
-import { MOCK_COURSES } from "@/constants/mockdata";
+import { useEffect } from "react";
+// import { Warning } from "@mui/icons-material";
+// import { Button } from "@mui/material";
+// import CourseInfo, { type CourseInfoProps } from "@/components/course/CourseInfo";
+// import UserReview from "@/components/course/UserReview";
 
 export default function CourseDetails({ params }: { params: { id: string } }) {
-  // params.id로 API fetch
-  const MOCK_DATA = MOCK_COURSES.find((course) => course.id === Number(params.id));
-  if (!MOCK_DATA)
-    return (
-      <div className="flex flex-col gap-4 text-center">
-        <p>
-          <Warning /> 404
-        </p>
-        <p>[{params.id}] id를 가진 강의의 정보가 mock 데이터에 존재하지 않습니다.</p>
-      </div>
-    );
+  // const [courseData, setCourseData] = useState<CourseInfoProps | null>();
+  // const [userReviews, setUserReviews] = useState([]);
+
+  useEffect(() => {
+    const getCourseData = async () => {
+      const fetchedCourse = await fetch(`/api/courses?id=${params.id}`);
+      // const departmentId = fetchedCourse.departmentId.toString();
+      // const professorId = fetchedCourse.professorId.toString();
+      // const fetchedDepartment = await fetchData(`/api/departments?id=${departmentId}`);
+      // const fetchedProfessor = await fetchData(`/api/departments?id=${professorId}`);
+      // console.log(fetchedCourse);
+      // console.log(fetchedDepartment);
+      // console.log(fetchedProfessor);
+      console.log(fetchedCourse);
+      // setCourseData(fetchedCourse);
+    };
+
+    getCourseData();
+  }, []);
+
   return (
     <div className="flex flex-col w-[950px] gap-4">
-      <CourseInfo {...MOCK_DATA} />
+      {/* <CourseInfo {...courseData} />
       <div className="flex justify-between mt-8">
         <span>강의 후기 | 질의답변</span>
         <Button variant="contained">후기 등록</Button>
       </div>
       <div>
         <UserReview id={1} username="test" review="추천합니다" reviewDate="2024.08.01" />
-      </div>
+      </div> */}
     </div>
   );
 }
