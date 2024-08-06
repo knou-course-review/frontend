c4fa16064ce56e32e04f018b38c758b600b92f44
jbilee <julishahn@gmail.com>
2024-08-06 17:01:01 +0900

    feat: courses 라우트 핸들러에 단건 조회 옵션 추가 - searchParams 전달 여부에 따라 단건 조회 또는 전체 조회를 하도록 구현

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -1,15 +1,30 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { api } from "@/utils/api";
 
-export async function GET() {
-  try {
-    const res = await api.get("/api/v1/courses");
-    if (res.status === 200) {
-      const body = await res.json();
-      return NextResponse.json(body.data);
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const idQuery = searchParams.get("id");
+
+  if (idQuery) {
+    try {
+      const res = await api.get(`/api/v1/course/${idQuery}`);
+      if (res.status === 200) {
+        const body = await res.json();
+        return NextResponse.json(body.data);
+      }
+    } catch (e) {
+      console.log(e);
     }
-  } catch (e) {
-    console.log(e);
+  } else {
+    try {
+      const res = await api.get("/api/v1/courses");
+      if (res.status === 200) {
+        const body = await res.json();
+        return NextResponse.json(body.data);
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
   return NextResponse.json({});
 }
