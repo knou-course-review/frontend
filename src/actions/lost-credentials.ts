eabdc4952427d1d3addc3215b429dbc3dbe96484
jbilee <julishahn@gmail.com>
2024-08-06 19:57:54 +0900

    feat: 아이디 찾기 관련 서버 액션 구현

---
+++ src/actions/lost-credentials.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { ErrorSchema } from "@/schema/error";
+import { api } from "@/utils/api";
+
+export async function getUsername(email: string) {
+  try {
+    const res = await api.post("/api/v1/users/find-username", { email });
+    if (res.ok) {
+      const body = await res.json();
+      return { isValid: true, data: body.data };
+    }
+  } catch (e) {
+    const isError = ErrorSchema.safeParse(e);
+    if (isError.success) {
+      console.log((e as Error).message);
+    }
+  }
+  return { isValid: false };
+}
+
+export async function sendCode(email: string) {
+  try {
+    const res = await api.post("/api/v1/mail", { email });
+    console.log(res);
+    if (res.ok) {
+      const body = await res.json();
+      console.log(body);
+      return { isValid: true };
+    }
+  } catch (e) {
+    const isError = ErrorSchema.safeParse(e);
+    if (isError.success) {
+      console.log((e as Error).message);
+    } else console.log(e);
+  }
+  return { isValid: false };
+}
+
+export async function checkCode(body: { email: string; code: string }) {
+  console.log(body);
+  try {
+    const res = await api.put("/api/v1/mail", body);
+    if (res.ok) {
+      const body = await res.json();
+      console.log(body);
+      return { isValid: body.data.confirm };
+    }
+  } catch (e) {
+    const isError = ErrorSchema.safeParse(e);
+    if (isError.success) {
+      console.log((e as Error).message);
+    } else console.log(e);
+  }
+  return { isValid: false };
+}
