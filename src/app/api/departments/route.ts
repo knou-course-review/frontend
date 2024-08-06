00a45ac89e1bf5cfa468e4fde5c2481a9dd48a7e
jbilee <julishahn@gmail.com>
2024-08-06 17:12:44 +0900

    feat: professors 및 departments 라우트 핸들러 구현

---
+++ src/app/api/departments/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { api } from "@/utils/api";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const idQuery = searchParams.get("id");
+
+  if (idQuery) {
+    try {
+      const res = await api.get(`/api/v1/department/${idQuery}`);
+      if (res.status === 200) {
+        const body = await res.json();
+        return NextResponse.json(body.data);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    try {
+      const res = await api.get("/api/v1/departments");
+      if (res.status === 200) {
+        const body = await res.json();
+        return NextResponse.json(body.data);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return NextResponse.json({});
+}
