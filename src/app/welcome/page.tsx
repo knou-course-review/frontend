b788a71532fd4c38c23cb7312a23a51c21cad012
jbilee <julishahn@gmail.com>
2024-08-07 00:07:40 +0900

    feat: 강의 목록 페이지네이션 기능 구현

--- src/app/welcome/page.tsx
+++
@@ -1,17 +0,0 @@
-import { Button } from "@mui/material";
-import Link from "next/link";
-
-export default function Welcome() {
-  return (
-    <div className="flex flex-col justify-between text-center h-100">
-      <h1 className="text-2xl font-bold">환영합니다!</h1>
-      <span className="text-6xl">🎉</span>
-      회원 가입이 완료되었습니다!
-      <br />
-      로그인 후 KNOU 서비스를 이용할 수 있습니다.
-      <Button variant="contained">
-        <Link href="/login">로그인하러 가기</Link>
-      </Button>
-    </div>
-  );
-}
--- src/components/course/CourseListContainer.tsx
+++ src/components/course/CourseListContainer.tsx
@@ -1,31 +1,31 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import CoursePreview, { type CoursePreviewProps } from "./CoursePreview";
+import { useState } from "react";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import CoursePreview from "./CoursePreview";
+import PageNavigator from "../PageNavigator";
+import type { CourseInfoProps } from "./CourseInfo";
+
+const fetchAllCourses = (page = 1) => fetch(`/api/courses?page=${page}`).then((res) => res.json());
 
 export default function CourseListContainer() {
-  const [courses, setCourses] = useState<CoursePreviewProps[]>([]);
-  useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const res = await fetch("/api/courses");
-        if (res.ok) {
-          const data = await res.json();
-          console.log(data);
-          setCourses(data);
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
+  const [page, setPage] = useState(1);
+  const { data, error } = useQuery({
+    queryKey: ["all-courses", page],
+    queryFn: () => fetchAllCourses(page),
+    placeholderData: keepPreviousData,
+  });
 
-    fetchCourses();
-  }, []);
+  const handlePageSelect = (value: number) => setPage(value);
+
+  if (error) return <div className="flex flex-col gap-4">에러가 발생했습니다!</div>;
+  if (!data) return <div className="w-full text-center">Loading ...</div>;
   return (
     <div className="flex flex-col gap-4">
-      {courses.map((course) => (
+      {data.content.map((course: CourseInfoProps) => (
         <CoursePreview key={course.id} {...course} />
       ))}
+      <PageNavigator currentPage={data.pageNumber} pages={data.totalPages} handlePageSelect={handlePageSelect} />
     </div>
   );
 }
