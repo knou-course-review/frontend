644ccd3b279f9acdde17778815467d2b1b43751a
jbilee <julishahn@gmail.com>
2024-08-07 00:28:00 +0900

    refactor: 강의 상세 정보 컴포넌트 리팩토링 - 변수명 최신화 작업

--- src/components/course/CourseInfo.tsx
+++ src/components/course/CourseInfo.tsx
@@ -1,34 +1,37 @@
 import Label from "../Label";
 
-type CourseInfoProps = {
+export type CourseInfoProps = {
+  id: number;
   courseName: string;
-  professor: string;
-  department: string;
+  professorName: string;
+  departmentName: string;
   classification: string;
-  year: string;
+  classType: string;
+  grade: number;
   semester: string;
+  credit: number;
 };
 
 export default function CourseInfo({
   courseName,
-  professor,
-  department,
+  professorName,
+  departmentName,
   classification,
-  year,
+  grade,
   semester,
 }: CourseInfoProps) {
   return (
     <div className="p-6 border border-neutral-400 rounded-2xl flex justify-between h-40">
       <div className="flex flex-col justify-between">
         <h1 className="mb-3 text-2xl font-semibold">{courseName}</h1>
-        <span>{professor}</span>
+        <span>{professorName}</span>
         <span>
-          {year} {semester}
+          {grade}학년 {semester}
         </span>
       </div>
       <div className="flex flex-col justify-between items-end">
         <div className="flex gap-4">
-          <Label text={department} background="bg-neutral-400" display="inline-block" />
+          <Label text={departmentName} background="bg-neutral-400" display="inline-block" />
           <Label text={classification} background="bg-neutral-400" display="inline-block" />
         </div>
       </div>
