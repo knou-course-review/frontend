0475d3892adf7cd64f9f59baca6dd2be9f18c1ec
jbilee <julishahn@gmail.com>
2024-08-12 10:43:20 +0900

    feat: 검색바에 검색 유형 선택지 추가 - 강의, 학과, 교수 searchType 중 하나를 선택하는 컴포넌트 구현

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,43 +1,39 @@
 "use client";
 
-import { type ChangeEvent, type FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
-import { IconButton, TextField } from "@mui/material";
 import Search from "@mui/icons-material/Search";
+import { IconButton, MenuItem, Select, TextField } from "@mui/material";
+import type { FormEvent } from "react";
 
-const searchPlaceholder = "강의, 학과 또는 교수님을 검색해보세요.";
+const searchPlaceholder = "강의, 학과 또는 교수님을 검색해 보세요.";
 
 export default function SearchBar() {
   const router = useRouter();
-  const [input, setInput] = useState("");
 
-  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    const input = e.target.value;
-    if (input.length > 50) return;
-    setInput(input);
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    handleSearch();
-  };
-
-  const handleSearch = () => {
-    if (input === "") return;
-    router.push(`/search?query=${input}`); // 엔드포인트 미정
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const name = formData.get("name");
+    const searchType = formData.get("searchType");
+    router.push(`/search?searchType=${searchType}&name=${name}`);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-full">
+    <form onSubmit={handleSubmit} className="flex w-full gap-2">
+      <Select name="searchType" size="small" defaultValue="courseName" autoWidth>
+        <MenuItem value="courseName">강의 검색</MenuItem>
+        <MenuItem value="departmentName">학과 검색</MenuItem>
+        <MenuItem value="professorName">교수 검색</MenuItem>
+      </Select>
       <TextField
         variant="standard"
-        value={input}
-        onChange={handleInput}
+        name="name"
         placeholder={searchPlaceholder}
         fullWidth
         InputProps={{
           endAdornment: (
-            <IconButton onClick={handleSearch}>
+            <IconButton type="submit">
               <Search />
             </IconButton>
           ),
