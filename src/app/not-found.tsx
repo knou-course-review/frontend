f8d61164b30282cfd7a128ba95e36445ca3ad605
jbilee <julishahn@gmail.com>
2024-08-22 10:39:47 +0900

    feat: 약관 및 404 페이지 구현 - 서비스 이용약관 페이지 구현 - 개인정보처리방침 페이지 구현 - 404 오류 페이지 구현

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">404 NOT FOUND</h1>
+      <p className="mt-4">페이지가 존재하지 않습니다.</p>
+    </div>
+  );
+}
